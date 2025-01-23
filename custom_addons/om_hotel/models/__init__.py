0bc163579eac0a00504e44fcecc0813f547c722c
Lê Chính Đại <140992360+RelliZisme98@users.noreply.github.com>
2025-01-23 17:49:03 +0700

    challenge a Hoàng final
    challenge a Hoàng final

--- custom_addons/om_hotel/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import hotel, booking, room, room_feature, hotel_management, booking_payment_wizard, hotel_service_line, hotel_service, hotel_sale_order, hotel_report_wizard, pos_integration, product_extend, hotel_report
